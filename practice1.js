cc6ccfcfed083abd714091ac53c31621febb4bc9
Ynel <ynel0406@gmail.com>
2026-05-21 17:23:27 +0800

    Problem 1

---
+++ practice1.js
@@ -0,0 +1,10 @@
+function sumPositive(numArr) {
+    let sum = 0;
+    for (let i = 0; i < numArr.length; i++) {
+        const number = numArr[i];
+        if (number > 0) sum += number;
+    }
+    return sum;
+}
+
+console.log(sumPositive([1, -2, 3, 4, -5]));
